2ac523ce18887f73bb77f10800eb5d97be25e2e5
dlee3427 <david.lee.sl2634@yale.edu>
2020-05-28 13:59:07 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+require 'pry'
+class Artist 
+    attr_accessor :name
+
+    @@all = []
+
+    def self.all 
+        @@all 
+    end 
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def songs 
+        Song.all.select {|song| song.artist == self}
+    end 
+
+    def add_song(song)
+        song.artist = self 
+    end 
+
+    def add_song_by_name(name)
+        song = Song.new(name) 
+        song.artist = self
+    end
+
+    def self.song_count
+        Song.all.count
+    end
+
+end
+
+# adele = Artist.new("Adele")
+# rolling_in_the_deep = Song.new("Rolling in the Deep")
+
+# rolling_in_the_deep.artist = adele 
